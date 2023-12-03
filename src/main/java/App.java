dba6e6ed078b58439ac7c5a21890747624579817
SchaninAlexander <129684702+SchaninAlexander@users.noreply.github.com>
2023-12-03 04:34:14 +0300

    Spring Hello World

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,32 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        HelloWorld helloWorld = applicationContext.getBean("helloworld",HelloWorld.class);
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Ссылки hw равны? - " + (bean == bean1));
+        System.out.println("Ссылки cat равны? - " + (bean2 == bean3));
+
+
+
     }
 }
